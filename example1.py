3a50700a95c2be5e192e20907296ef8c8d3ecdc5
aayush <aayushjn2@gmail.com>
2017-02-03 01:32:38 +0530

    testing only

--- example1.py
+++ example1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello():
 <<<<<<< HEAD
-    return 'Hello world..!!!'
+    return 'Hello world...!!!'
 =======
     return 'Hello world <b>python</b>'
 >>>>>>> e85f3cd1422a9fdbab2a13c97f19079d6b446361
