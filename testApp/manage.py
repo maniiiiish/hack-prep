0a2340de14108376cb0f3b07464ae130e0601fa2
Utkarsh Raj <clearnote01@gmail.com>
2017-02-04 02:06:59 +0530

    learning flask

--- testApp/manage.py
+++ testApp/manage.py
@@ -1,17 +1,17 @@
 import click
 
 @click.command()
-@click.option(help='Start flask server')
 def rs():
     click.echo('Starting server')
     from app import app
     app.run()
 
 @click.command()
-@click.option(help='Create tables in database')
 def ct():
     click.echo('Creating tables')
-    import models
+    from models import db
+    from app import db
+    db.create_all()
 
 @click.group()
 def cli():
@@ -20,5 +20,6 @@ def cli():
 if __name__=='__main__':
     cli.add_command(ct)
     cli.add_command(rs)
+    cli()
 
 
